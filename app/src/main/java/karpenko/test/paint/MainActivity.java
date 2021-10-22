853691bd0ecc3c45ef4518a526742fdb85dec77b
SkyLeeNeci <92163391+SkyLeeNeci@users.noreply.github.com>
2021-10-22 17:38:51 +0300

    added color picker added eraser added pencil size selection

--- app/src/main/java/karpenko/test/paint/MainActivity.java
+++ app/src/main/java/karpenko/test/paint/MainActivity.java
@@ -1,14 +1,118 @@
 package karpenko.test.paint;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.os.Environment;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.kyanogen.signatureview.SignatureView;
+
+import java.io.File;
+import java.util.List;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String fileName;
+    File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Paint");
+
+    SignatureView signatureView;
+    ImageButton eraserImg, colorImg, saveImg;
+    SeekBar seekBar;
+    TextView penSizeInfo;
+
+    int defaultColor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signatureView = findViewById(R.id.signature_view);
+        seekBar = findViewById(R.id.penSize);
+        penSizeInfo = findViewById(R.id.penSizeInfo);
+        eraserImg = findViewById(R.id.eraserBtn);
+        colorImg = findViewById(R.id.colorBtn);
+        saveImg = findViewById(R.id.saveBtn);
+
+        askStoragePermission();
+
+        defaultColor = ContextCompat.getColor(MainActivity.this,R.color.black);
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                penSizeInfo.setText(progress + "");
+                signatureView.setPenSize(progress);
+                seekBar.setMax(150);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        eraserImg.setOnClickListener(v -> {
+            signatureView.clearCanvas();
+        });
+
+        colorImg.setOnClickListener(v -> {
+            openColorPicker();
+        });
+
+    }
+
+
+
+    private void openColorPicker() {
+        AmbilWarnaDialog ambilWarnaDialog = new AmbilWarnaDialog(this, defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColor = color;
+                signatureView.setPenColor(color);
+            }
+        });
+        ambilWarnaDialog.show();
+    }
+
+    private void askStoragePermission() {
+
+        Dexter.withContext(this).withPermissions(Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        .withListener(new MultiplePermissionsListener() {
+            @Override
+            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+                Toast.makeText(MainActivity.this,"Granted",Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                permissionToken.cancelPermissionRequest();
+            }
+        }).check();
+
     }
 }
